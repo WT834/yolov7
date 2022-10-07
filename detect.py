506300fb5d220ceba31772520ac1631d7378dc9c
WT834 <91351565+WT834@users.noreply.github.com>
2022-10-07 14:26:03 +0100

    Update detect.py

--- detect.py
+++ detect.py
@@ -120,9 +120,9 @@ def detect(save_img=False):
                     
                     if save_txt:  # Write to file
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if opt.save_conf else ({frame}, *xywh)  # label format
+                        line = (cls, *xywh, conf) if opt.save_conf else ({cls, *xywh)  # label format
                         with open(txt_path + '.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                            f.write({frame} + ('%g ' * len(line)).rstrip() % line + '\n')
 
                     if save_img or view_img:  # Add bbox to image
                         label = f'{names[int(cls)]} {conf:.2f}'
